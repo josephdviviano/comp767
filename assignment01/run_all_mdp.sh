870ee09d4279202425e77d25c0748b5e38bbb169
Joseph Viviano <joseph@viviano.ca>
2019-01-28 23:57:58 -0500

    Adding plots

--- assignment01/run_all_mdp.sh
+++ assignment01/run_all_mdp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # p=0.7, grid=5x5
-./mdp.py --iteration policy
-./mdp.py --iteration modified -k 25
-./mdp.py --iteration value
+#./mdp.py --iteration policy
+#./mdp.py --iteration modified -k 25
+#./mdp.py --iteration value
 
 # p=0.9, grid=5x5
-./mdp.py --iteration policy -p 0.9
-./mdp.py --iteration modified -k 25 -p 0.9
-./mdp.py --iteration value -p 0.9
+#./mdp.py --iteration policy -p 0.9
+#./mdp.py --iteration modified -k 25 -p 0.9
+#./mdp.py --iteration value -p 0.9
 
 # p=0.7, grid=50x50
 ./mdp.py --iteration policy --size 50
