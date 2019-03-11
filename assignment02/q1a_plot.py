ccc2034fc56537e1a836353a7490581af634540a
Joseph Viviano <joseph@viviano.ca>
2019-03-11 17:36:44 -0400

    now plots each learning curve with the best found hyperparameters for each algorithms

--- assignment02/q1a_plot.py
+++ assignment02/q1a_plot.py
@@ -26,11 +26,21 @@ def plot_hp_comparisons(split):
     YLIM = [-500, 50]
     TITLES = ['Sarsa', 'Expected Sarsa', 'Q Learning']
 
+    # Stores best hyperparameters for each experiment
+    # [(Alpha1, Temp1), (Alpha2, Temp2), (Alpha3, Temp3)].
+    hyperparameters = []
+
     f, axs = plt.subplots(3, 1, sharex=True, figsize=(6, 12))
 
     for i, ax in enumerate(axs):
 
-        ax.plot(np.mean(split.best_seg()[i, :, :, :], axis=2))
+        results = np.mean(split.best_seg()[i, :, :, :], axis=2)
+
+        # Get hyperparametes for maximum return.
+        alpha, temp = np.where(results == np.max(results))
+        hyperparameters.append([alpha[0], temp[0]])
+
+        ax.plot(results)
         ax.set_ylabel('Return')
         ax.set_xticks(XTICKS)
         ax.set_ylim(YLIM)
@@ -47,34 +57,43 @@ def plot_hp_comparisons(split):
 
     f.savefig('img/{}_alpha_temp_return.jpg'.format(split.name))
 
+    return(hyperparameters)
 
-def plot_learning_curves(train, test):
+
+def plot_learning_curves(train, test, hyperparameters):
 
     LEGEND = ['train', 'test']
     X = np.arange(100)
     XTICKS = np.arange(0, 101, 10)
-    XTICKLABELS = XTICKS
+    XTICKLABELS = np.arange(0, 101, 10)
     XTICKLABELS[0] = 1
     YLIM = [-1000, 200]
-    ALPHA = 2
-    TEMP = 1
+    ALPHAS = ['0.1', '0.3', '0.5', '0.7', '0.9']
+    TEMPS = ['0.1', '1.0', '10']
     TITLES = ['Sarsa', 'Expected Sarsa', 'Q Learning']
 
     f, axs = plt.subplots(3, 1, sharex=True, figsize=(6, 12))
 
     for i, ax in enumerate(axs):
+
+        alpha, temp = hyperparameters[i]
+
         ax.errorbar(X,
-                     train.run_stats()[0][i, ALPHA, TEMP, :],
-                     train.run_stats()[1][i, ALPHA, TEMP, :])
+                     train.run_stats()[0][i, alpha, temp, :],
+                     train.run_stats()[1][i, alpha, temp, :])
         ax.errorbar(X,
-                     test.run_stats()[0][i, ALPHA, TEMP, :],
-                     test.run_stats()[1][i, ALPHA, TEMP, :])
+                     test.run_stats()[0][i, alpha, temp, :],
+                     test.run_stats()[1][i, alpha, temp, :])
         ax.set_ylabel('Return')
         ax.set_xticks(XTICKS)
         ax.set_xticklabels([])
         ax.set_ylim(YLIM)
         ax.legend(LEGEND)
-        ax.set_title(TITLES[i])
+
+        title = "{}: alpha={}, temp={}".format(
+            TITLES[i], ALPHAS[alpha], TEMPS[temp])
+
+        ax.set_title(title)
 
         if i+1 == len(axs):
             ax.set_xticklabels(XTICKLABELS)
@@ -93,10 +112,10 @@ def main():
     train = RunStats(data.item(), 'train')
     test = RunStats(data.item(), 'test')
 
-    plot_hp_comparisons(train)
-    plot_hp_comparisons(test)
+    _ = plot_hp_comparisons(train)
+    hyperparameters = plot_hp_comparisons(test)
 
-    plot_learning_curves(train, test)
+    plot_learning_curves(train, test, hyperparameters)
 
 
 if __name__ == "__main__":
